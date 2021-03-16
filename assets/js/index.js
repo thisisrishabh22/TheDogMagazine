4a9b57b6cda5ae84f97240c6acd61bfa8567614d
thisisrishabh22 <ris8645208@gmail.com>
2021-03-16 16:32:36 +0530

    new

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.header').addClass("sticky");
+        }else{
+            $('.header').removeClass("sticky");
+        }
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+        $('.menu-btn').toggleClass("active");
+    });
+});
